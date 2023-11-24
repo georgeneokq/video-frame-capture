5246d0865b98e63544ce05f5c78509eea6a78b95
George <george.neokq@gmail.com>
2023-11-24 14:57:37 +0800

    change command line arguments

--- README.md
+++ README.md
@@ -4,6 +4,12 @@ A simple console program to capture and export the frames of a video.
 
 ## Usage
 
+{FILE_PATH} is a required positional argument.
+
+`-c` is an optional argument specifying frames to capture per second. Default: 2
+
+`-q` is an optional argument, specifying quality of frames exported, in percentage. Default: 100
+
 ```
-python main.py -f {FILE_PATH}
+python main.py {FILE_PATH} -c {CAPTURES_PER_SECOND}
 ```
--- main.py
+++ main.py
@@ -19,10 +19,12 @@ def get_full_timestamp(millis):
 
 # Read the video from specified path
 parser = argparse.ArgumentParser()
-parser.add_argument('-f', '--file', type=str, required=True)
+parser.add_argument('file', type=str)
 parser.add_argument('-c', '--captures-per-second', type=int, default=2)
+parser.add_argument('-q', '--quality', type=int, default=100)
 args = parser.parse_args()
 file_path = args.file
+quality = args.quality
 cam = cv2.VideoCapture(file_path)
 
 try:
@@ -40,12 +42,16 @@ with open(file_path, "rb") as f:
 
 # frame
 current_frame = 0
-captures_per_second = args.captures_per_second
 frames_per_second = round(cam.get(cv2.CAP_PROP_FPS) / 10) * 10
+
+if args.captures_per_second > frames_per_second:
+    print(f'Specified frames to capture per second exceeds FPS; capturing the maximum possible of {frames_per_second} frames per second.')
+captures_per_second = min(args.captures_per_second, frames_per_second)
 millisecs_per_frame = 1000 / frames_per_second
-frames_to_skip = (frames_per_second - (frames_per_second % captures_per_second)) // captures_per_second
+frames_to_jump = (frames_per_second - (frames_per_second % captures_per_second)) // captures_per_second
 print(f'Frames per second: {frames_per_second}')
-print(f'Captures per second: {captures_per_second}, frames to skip: {frames_to_skip}')
+print(f'Captures per second: {captures_per_second}\nFrames to jump per iteration: {frames_to_jump}')
+print(f'Quality of exported frames: {quality}%')
 
 while (True):
     # reading from frame
@@ -62,10 +68,10 @@ while (True):
         print(f'Creating {name}')
 
         # writing the extracted images
-        cv2.imwrite(name, frame, [cv2.IMWRITE_JPEG_QUALITY, 50])
+        cv2.imwrite(name, frame, [cv2.IMWRITE_JPEG_QUALITY, quality])
 
         # Skip frames according to how many captures per second were specified
-        current_frame += frames_to_skip
+        current_frame += frames_to_jump
         cam.set(cv2.CAP_PROP_POS_FRAMES, current_frame)
     else:
         break
